6ce3112923f3d7c95a8369b564c3deea070690f0
ERROR22542 <arkadiusz@jarnecki.pl>
2022-10-28 18:08:29 +0200

    commit

--- main/script.js
+++ main/script.js
@@ -1,19 +1,34 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+"use strict";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDjPD1K6MjsIvOlZaIHloeC9lfSG10p-jw",
-  authDomain: "top-grass-club.firebaseapp.com",
-  projectId: "top-grass-club",
-  storageBucket: "top-grass-club.appspot.com",
-  messagingSenderId: "920676001797",
-  appId: "1:920676001797:web:307e9daec7ea687093146c",
-};
+const mobileBg = document.querySelector("header");
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const leftBgSwitch = document.querySelector(".mobile__view--switcher1");
+const centerBgSwitch = document.querySelector(".mobile__view--switcher2");
+const rightBgSwitch = document.querySelector(".mobile__view--switcher3");
 
-initializeApp(firebaseConfig);
+leftBgSwitch.addEventListener("click", () => {
+  mobileBg.classList.remove("right");
+  mobileBg.classList.remove("center");
+  mobileBg.classList.add("left");
+  leftBgSwitch.classList.add("active");
+  centerBgSwitch.classList.remove("active");
+  rightBgSwitch.classList.remove("active");
+});
+
+centerBgSwitch.addEventListener("click", () => {
+  mobileBg.classList.remove("right");
+  mobileBg.classList.add("center");
+  mobileBg.classList.remove("left");
+  leftBgSwitch.classList.remove("active");
+  centerBgSwitch.classList.add("active");
+  rightBgSwitch.classList.remove("active");
+});
+
+rightBgSwitch.addEventListener("click", () => {
+  mobileBg.classList.add("right");
+  mobileBg.classList.remove("center");
+  mobileBg.classList.remove("left");
+  leftBgSwitch.classList.remove("active");
+  centerBgSwitch.classList.remove("active");
+  rightBgSwitch.classList.add("active");
+});
