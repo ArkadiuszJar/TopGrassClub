b496abc413c3bbcd2ba8016c59257adc1772416e
ERROR22542 <arkadiusz@jarnecki.pl>
2022-10-28 12:13:26 +0200

    update

---
+++ main/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjPD1K6MjsIvOlZaIHloeC9lfSG10p-jw",
+  authDomain: "top-grass-club.firebaseapp.com",
+  projectId: "top-grass-club",
+  storageBucket: "top-grass-club.appspot.com",
+  messagingSenderId: "920676001797",
+  appId: "1:920676001797:web:307e9daec7ea687093146c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
